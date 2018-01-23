e67f7ad8d717c3e85c9e54662d85e88f831aa887
danl411412 <daan.b.lee@gmail.com>
2018-01-23 14:02:30 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,8 +23,9 @@ def get_user_input
   return gets.chomp
 end
 
-def end_game
+def end_game(num)
   # code #end_game here
+  puts "Sorry, you hit #{num}. Thanks for playing!"
 end
 
 def initial_round
